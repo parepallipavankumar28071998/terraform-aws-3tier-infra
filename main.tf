ff31cef7d9dcc5b406d3be2b26d269862d178229
parepallipavankumar28071998 <134995724+parepallipavankumar28071998@users.noreply.github.com>
2026-08-13 19:19:26 +0530

    Attach IAM profile to EC2 instance

--- main.tf
+++ main.tf
@@ -64,6 +64,7 @@ resource "aws_instance" "web" {
   instance_type           = var.instance_type
   subnet_id               = aws_subnet.public.id
   vpc_security_group_ids  = [aws_security_group.web_sg.id]
+  iam_instance_profile    = aws_iam_instance_profile.ec2_profile.name
 
   tags = {
     Name = "web-server"
